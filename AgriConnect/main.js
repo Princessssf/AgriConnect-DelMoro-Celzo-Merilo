3dd9e69f74dd9eb3fc6c9eb33269fe5c6b22ee5a
Joru445 <joru445@gmail.com>
2025-10-10 11:25:17 +0800

    Update main.js. Kita ko yon sir

--- AgriConnect/main.js
+++ AgriConnect/main.js
@@ -2,7 +2,8 @@ const buttons = document.querySelectorAll('.navigation button');
 const pages = document.querySelectorAll('.page');
 
 buttons.forEach(btn => {
-  btn.addEventListener('click', () => 
+  btn.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
 
     pages.forEach(p => p.classList.remove('active'));
